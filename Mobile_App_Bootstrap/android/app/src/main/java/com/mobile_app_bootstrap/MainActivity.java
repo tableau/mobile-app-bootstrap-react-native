97959ee4d93230fc05262ee5cb6ee297caee30d7
Ron Theis <rtheis@tableau.com>
2018-07-09 16:20:34 -0700

    After running eject

---
+++ Mobile_App_Bootstrap/android/app/src/main/java/com/mobile_app_bootstrap/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mobile_app_bootstrap;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Mobile_App_Bootstrap";
+    }
+}
