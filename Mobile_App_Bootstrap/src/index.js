b72e5f3426d4dafc54a0d04b06f67420cb360295
Ron Theis <rtheis@tableau.com>
2018-07-10 10:03:46 -0700

    Remove some typescript artifacts

--- Mobile_App_Bootstrap/src/index.js
+++ Mobile_App_Bootstrap/src/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import { RootNavigator } from './router'
 
-export default class App extends React.Component<> {
+export default class App extends React.Component {
     render() {
         return (
             <RootNavigator />
