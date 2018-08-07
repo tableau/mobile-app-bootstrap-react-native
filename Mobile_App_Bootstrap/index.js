e75f438d7cb72d6f651a14aff616f4150b29d3cb
Ron Theis <rtheis@tableau.com>
2018-08-07 15:50:54 -0700

    Updated to RN 0.55.2, latest react, react-navigation

--- Mobile_App_Bootstrap/index.js
+++ Mobile_App_Bootstrap/index.js
@@ -1,3 +1,5 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
 AppRegistry.registerComponent('Mobile_App_Bootstrap', () => App);
+
+YellowBox.ignoreWarnings(['Warning: isMounted', 'Module RCTImageLoader']);
