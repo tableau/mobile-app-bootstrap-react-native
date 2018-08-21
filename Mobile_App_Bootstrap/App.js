e51bb495dc2650ddd0c0e899e1719cd1a55118d9
Ron Theis <rtheis@tableau.com>
2018-08-21 10:46:13 -0700

    Update the docs, use WKWebView everywhere

--- Mobile_App_Bootstrap/App.js
+++ Mobile_App_Bootstrap/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { RootNavigator } from './src/router';
 
 export default class App extends React.Component {
