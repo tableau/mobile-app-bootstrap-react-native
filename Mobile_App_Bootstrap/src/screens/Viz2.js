8e548eadeeb6c6ee28da448e8e5fe3e84996ea81
Ron Theis <rtheis@tableau.com>
2018-08-21 09:46:39 -0700

    Use WKWebView on iOS

--- Mobile_App_Bootstrap/src/screens/Viz2.js
+++ Mobile_App_Bootstrap/src/screens/Viz2.js
@@ -2,21 +2,32 @@ import * as React from 'react';
 import {
     Platform,
     StyleSheet,
-    Text,
     View,
     WebView
 } from 'react-native';
+import WKWebView from 'react-native-wkwebview-reborn';
 
 export default class Viz2 extends React.Component {
     render() {
-        return (
-            <View style={styles.container}>
-                <WebView
-                    source={{uri: 'https://public.tableau.com/views/10_0ClinicAnalytics/ClinicAnalytics?:embed=y&:tooltip=n&:toolbar=n&:showVizHome=no'}}
-                    style={styles.webview}
-                />
-            </View>
-        );
+        if (Platform.OS === 'android') {
+            return (
+                <View style={styles.container}>
+                    <WebView
+                        source={{uri: 'https://public.tableau.com/views/10_0ClinicAnalytics/ClinicAnalytics?:embed=y&:tooltip=n&:toolbar=n&:showVizHome=no'}}
+                        style={styles.webview}
+                    />
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.container}>
+                    <WKWebView
+                        source={{uri: 'https://public.tableau.com/views/10_0ClinicAnalytics/ClinicAnalytics?:embed=y&:tooltip=n&:toolbar=n&:showVizHome=no'}}
+                        style={styles.webview}
+                    />
+                </View>
+            );
+        }
     }
 }
 
